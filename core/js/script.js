36a1e012ea7f4a96ee6452fb17ea0c026b632141
Taylor Daughtry <hello@taylordaughtry.com>
2016-07-02 23:33:47 -0500

    Store map on public object

--- core/js/script.js
+++ core/js/script.js
@@ -9,21 +9,20 @@ Wee.fn.make('Cartographer', {
 	 * @return void
 	 */
 	init: function(setup) {
-		var priv = this.$private,
-			providerOptions = {
+		var providerOptions = {
 				attribution: Wee.view.render('cartographer.attribution'),
 				id: setup.projectId,
 				accessToken: setup.accessToken
 			};
 
-		priv.map = L.map(setup.id).setView(setup.startPoint, setup.startZoom);
+		this.map = L.map(setup.id).setView(setup.startPoint, setup.startZoom);
 
-		priv.map.attributionControl.setPrefix(false);
+		this.map.attributionControl.setPrefix(false);
 
 		L.Icon.Default.imagePath = '/assets/modules/cartographer/img/';
 
 		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', providerOptions)
-			.addTo(priv.map);
+			.addTo(this.map);
 	}
 }, {
 	method: function() {
